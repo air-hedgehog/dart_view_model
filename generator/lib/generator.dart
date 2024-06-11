82cffc2389eb901ad149b6a7bd926ec46e29e184
Antony Akimchenko <akimchenko.antony@gmail.com>
2024-06-11 23:16:35 +0400

    forced to use sharedPartBuilder

--- generator/lib/generator.dart
+++ generator/lib/generator.dart
@@ -4,7 +4,6 @@ import 'package:build/build.dart';
 import 'package:generator/src/model_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
-Builder generateModelGenClass(BuilderOptions options) => PartBuilder(
-      [ModelGenerator()],
-      ".state",
-    );
+Builder generateModelGenClass(BuilderOptions options) =>
+    SharedPartBuilder([ModelGenerator()], ".state",
+        formatOutput: (str) => "g.dart");

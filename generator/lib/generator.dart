0b6651aca39758f1b10abbc5b59a65070986654f
Anton Akimchenko <akimchenko.antony@icloud.com>
2025-10-29 16:38:45 +0400

    removed screen generator

--- generator/lib/generator.dart
+++ generator/lib/generator.dart
@@ -2,15 +2,7 @@ library generator;
 
 import 'package:build/build.dart';
 import 'package:generator/src/model_generator.dart';
-import 'package:generator/src/screen_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
 Builder generateModelGenClass(BuilderOptions options) =>
     SharedPartBuilder([ModelGenerator()], "state");
-
-Builder generateScreenFiles(BuilderOptions options) =>
-    LibraryBuilder(
-      ScreenGenerator(),
-      generatedExtension: '.screen.dart',
-      allowSyntaxErrors: true,
-    );

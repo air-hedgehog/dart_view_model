9bde15b8717cd4cf7e7d23c2b123ac8531579e29
Anton Akimchenko <akimchenko.antony@icloud.com>
2025-10-29 16:48:29 +0400

    updated libs

--- generator/lib/src/model_visitor.dart
+++ generator/lib/src/model_visitor.dart
@@ -1,26 +1,19 @@
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/dart/element/visitor.dart';
+import 'package:analyzer/dart/element/visitor2.dart';
 
-class ViewModelVisitor extends SimpleElementVisitor<void> {
+class ViewModelVisitor extends SimpleElementVisitor2<void> {
   String className = '';
   Map<String, dynamic> fields = {};
 
   @override
   void visitConstructorElement(ConstructorElement element) {
     final String returnType = element.returnType.toString();
-    className = returnType.replaceAll("*", ""); // ClassName* -> ClassName
+    className = returnType.replaceAll("*", "");
   }
 
   @override
   void visitFieldElement(FieldElement element) {
-    /*
-    {
-      name: String,
-      price: double
-    }
-     */
-
     String elementType = element.type.toString().replaceAll("*", "");
-    fields[element.name] = elementType;
+    fields[element.name ?? ""] = elementType;
   }
 }
